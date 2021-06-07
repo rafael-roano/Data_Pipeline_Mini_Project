90791edd7967a3e316beab2563757830c3c71da4
rafael-roano <rafael.roano@gmail.com>
2021-06-06 18:43:00 -0700

    Project 2.0 commit

--- ticket_sales_table_DDL.sql
+++ ticket_sales_table_DDL.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS ticket_sales (
     ticket_id INT,
-    trans_date INT,
+    trans_date DATE,
     event_id INT,
     event_name VARCHAR(50),
     event_date DATE,
     event_type VARCHAR(50),
     event_city VARCHAR(20),
     customer_id INT,
-	price DECIMAL,
+	price DECIMAL(10,2),
     num_tickets INT
 );
